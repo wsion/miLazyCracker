e149abd225f5702fec4b3d7854c16f67fac769fa
wsion <waychanmail@gmail.com>
2019-01-12 18:30:50 +0800

    Remove patch of mfoc

--- miLazyCrackerFreshInstall.sh
+++ miLazyCrackerFreshInstall.sh
@@ -13,10 +13,10 @@ sudo apt-get install git libnfc-bin autoconf libnfc-dev
     git reset --hard
     git clean -dfx
     # patch initially done against commit 48156f9b:
-    patch -p1 < ../mfoc_test_prng.diff
-    patch -p1 < ../mfoc_fix_4k_and_mini.diff
-    patch -p1 < ../mfoc_support_tnp.diff
-    patch -p1 < ../mfoc_support_2k.diff
+    #patch -p1 < ../mfoc_test_prng.diff
+    #patch -p1 < ../mfoc_fix_4k_and_mini.diff
+    #patch -p1 < ../mfoc_support_tnp.diff
+    #patch -p1 < ../mfoc_support_2k.diff
     autoreconf -vfi
     ./configure
     make
